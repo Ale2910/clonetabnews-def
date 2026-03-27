340a4ff7b957d2449e4448875dcff33e6f8b505f
Alehandro <104379596+Ale2910@users.noreply.github.com>
2026-03-27 13:46:56 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
-    return [<h1>Eu te amo, minha vida 💙</h1>, 
-            <h2>Agora você me deve uma beijoca 🤭</h2>]
+  return [
+    <h1>Eu te amo, minha vida 💙</h1>,
+    <h2>Agora você me deve uma beijoca 🤭</h2>,
+  ];
 }
 
 export default Home;
